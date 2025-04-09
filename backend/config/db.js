8ad11bf072e52838c2a7906209f24ced30acb9ba
Humoyunbek Tursunniyazov <154880540+humoyun-dev@users.noreply.github.com>
2025-04-09 18:38:11 +0500

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://mongo_user:mongo_password@fast-food.ehtuq.mongodb.net/fast-food').then(()=>console.log("DataBase Connected"));
+    await mongoose.connect('mongodb+srv://baxtiyorovmuhammadaziz28:main1234@cluster0.bf5odhj.mongodb.net/test').then(()=>console.log("DataBase Connected"));
 }
